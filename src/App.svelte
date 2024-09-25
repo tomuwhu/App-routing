0304a15889bae50e10ba9d81b2f4e799056fcc0e
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-25 09:11:29 +0200

    .

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  // @ts-ignore
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+  export let url = ""
+</script>
+
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+  </nav>
+    <main>
+      <Route path="/about" component={About} />
+      <Route path="/" component={Home} />
+    </main>
+</Router>
+
+<style>
+
+</style>
